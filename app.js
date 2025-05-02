0768e964f32a57c17e99473e1db098c646863ba8
brt06a <brt06a@gmail.com>
2025-05-02 08:59:19 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// Handle Sign Up Button
+document.querySelector('.signup-btn').addEventListener('click', () => {
+  window.location.href = "signup.html";  // or open signup modal
+});
+
+// Handle Contact Button
+document.querySelector('.contact-btn').addEventListener('click', () => {
+  alert("Contact us at support@e-online.com");
+});
+
+// Handle Browse All
+document.querySelector('.browse-btn').addEventListener('click', () => {
+  alert("Loading all available courses...");
+});
