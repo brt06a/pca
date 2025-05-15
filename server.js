3bef69ef7d10b4b7de970604339ed18e77730888
Biswa Ranjan Tudu <brt06a@gmail.com>
2025-05-15 10:01:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/chat', async (req, res) => {
+  const messages = req.body.messages;
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages
+    });
+
+    res.json({ reply: response.data.choices[0].message.content });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
